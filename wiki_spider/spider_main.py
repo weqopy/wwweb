885b3d9c6783225eddf92938cb7f41df2db983d2
weqopy <weqopy@gmail.com>
2017-10-31 10:01:27 +0800

    Update spider_main.py
    分析主模块

--- wiki_spider/spider_main.py
+++ wiki_spider/spider_main.py
@@ -8,20 +8,26 @@ class SpiderMain(object):
     """docstring for SpiderMain"""
 
     def __init__(self):
+        # 引入相应模块中的类
         self.urls = url_manager.UrlManager()
         self.downloader = html_downloader.HtmlDownLoader()
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
 
     def craw(self, root_url):
+        # 爬取页面数量
         count = 1
         self.urls.get_new_url(root_url)
         while self.urls.has_new_url():
             try:
                 new_url = self.urls.get_new_url(root_url)
+                # 显示当前爬取数、链接
                 print('Craw {}: {}'.format(count, new_url))
+                # 下载页面
                 html_cont = self.downloader.download(new_url)
+                # 解析页面
                 new_urls, new_data = self.parser.parse(new_url, html_cont)
+                # 将新页面中的新链接存入 urls
                 self.urls.add_new_urls(new_urls)
                 self.outputer.collect_data(new_data)
 
@@ -30,7 +36,7 @@ class SpiderMain(object):
                 count += 1
             except:
                 print('Craw failed at {}: {}'.format(count, new_url))
-
+        # 输出
         self.outputer.out_html()
 
 
